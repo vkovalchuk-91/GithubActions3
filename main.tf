cdd799cbee217a78834d1fe2db8562a188bdcdff
Volodymyr Kovalchuk <102913549+vkovalchuk-91@users.noreply.github.com>
2024-11-21 16:41:41 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "AMI_ID" {
+  type = string
+  default = ${{ secrets.AMI_ID }}
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.AMI_ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AWS_TERRAFORM_TEST"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "security"
+  description = "Allow 80 port"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
